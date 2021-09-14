ec3e3674677fe3bfee31d6614bdabe0310785d54
Rahul <85161282+500apps-RahulVangari@users.noreply.github.com>
2021-09-14 15:18:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,10 @@
   s.async = 1;
   s.src = r;
   s.onload = async () => {
-    let d = decodeURIComponent(decodeURIComponent("ZGV2LTI4MTQtMTE5Ni0xMTEz-id"));
+    let v="ZGV2LTI4MTQtMTE5Ni0xMTEz-id";
+    let d = decodeURIComponent(decodeURIComponent(v.split('-')[0]));
     let r = window.atob(d);
-    var data = await f.wg.init("forms", r.split("-")[0], r.split("-")[1], r.split("-")[2], `forms/${r.split("-")[3]}/embed`, { width: "1200px", height: "750px" }, "widget", r.split("-")[4]);
+    var data = await f.wg.init("forms", r.split("-")[0], r.split("-")[1], r.split("-")[2], `forms/${r.split("-")[3]}/embed`, { width: "1200px", height: "750px" }, "widget", v.split("-")[1]);
   };
   m.appendChild(s);
   console.log("verified by script")
